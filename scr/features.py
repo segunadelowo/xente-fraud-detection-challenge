c6367abaa42ec27226b38e4eca32f7dd6bd5aecd
segunadelowo <adelowo@live.com>
2019-08-25 13:20:52 +0100

    project setup and analysis completion

--- scr/features.py
+++
@@ -1,5 +0,0 @@
-
-def drop_columns(df_features_train):
-    return df_features_train.drop(columns=['ChannelId_ChannelId_4','ProductCategory_retail','ProductId_ProductId_17',
-                                'ProductId_ProductId_18','ProductId_ProductId_25','ProductId_ProductId_26','FraudResult'])
-                                 
